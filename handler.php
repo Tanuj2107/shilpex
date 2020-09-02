7ba20156afa8e180e302bd0a2c985283b3dd6749
Tanuj2107 <63730282+Tanuj2107@users.noreply.github.com>
2020-09-03 00:14:30 +0530

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,26 +1,59 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
 
- */
-require_once './vendor/autoload.php';
+$errors = ”;
 
-use FormGuide\Handlx\FormHandler;
+$myemail = ‘tanujbajaj2191@gmail.com’;//<—–Put Your email address here. if(empty($_POST[‘name’]) ||
 
+empty($_POST[’email’]) ||
 
-$pp = new FormHandler(); 
+empty($_POST[‘message’]))
 
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('comments')->maxLength(6000);
+{
 
+$errors .= “\n Error: all fields are required”;
 
+}
 
+$name = $_POST[‘name’];
 
-$pp->sendEmailTo('tanujbajaj2191@gmail.com'); // ← Your email here
+$email_address = $_POST[’email’];
 
-echo $pp->process($_POST);
+$message = $_POST[‘message’];
+
+if (!preg_match(
+
+“/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i”, $email_address))
+
+{
+
+$errors .= “\n Error: Invalid email address”;
+
+}
+
+if( empty($errors))
+
+{
+
+$to = $myemail;
+
+$email_subject = “Contact form submission: $name”;
+
+$email_body = “You have received a new message. “.
+
+” Here are the details:\n Name: $name \n “.
+
+“Email: $email_address\n Message \n $message”;
+
+$headers = “From: $myemail\n”;
+
+$headers .= “Reply-To: $email_address”;
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the ‘thank you’ page
+
+header(‘Location: contact.html’);
+
+}
+
+?>
