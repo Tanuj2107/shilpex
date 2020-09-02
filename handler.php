0bd9f3275ca87d9176b03bc87411ac9e14ce16ba
Tanuj2107 <63730282+Tanuj2107@users.noreply.github.com>
2020-09-02 23:52:28 +0530

    Update handler.php

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('comments')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('sakshiadhikari@gmail.com'); // ← Your email here
+$pp->sendEmailTo('tanujbajaj2191@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
